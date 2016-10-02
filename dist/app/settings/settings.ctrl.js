713b41a0e1bf0723f71d9341285aa82bc57d6693
Tremainne <thomsondj@hotmail.com>
2016-10-02 01:37:17 -0700

    Working settings page with client list.

--- dist/app/settings/settings.ctrl.js
+++ dist/app/settings/settings.ctrl.js
@@ -5,6 +5,22 @@
   controller('SettingsController', ['$scope', '$state', '$window', 'AuthenticationService', SettingsController]);
 
   function SettingsController($scope, $state, $window, Authentication) {
-    
+  	$scope.clients = undefined;
+
+    $scope.loggedIn = Authentication.loggedIn;
+
+    if($scope.loggedIn) {
+      Authentication.allAddresses.on('value', getClientList);
+    }
+
+		function getClientList(onlineObj) {
+		  var clientList = onlineObj.val();
+
+		  var clientValues = Object.keys(clientList).map(function(key) {
+		    return clientList[key];
+		  });
+		  $scope.clients = clientValues;
+		  $scope.$apply();
+		}
   }
 })();
