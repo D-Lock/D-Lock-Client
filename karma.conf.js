bfa42c52d1e717eabfbc63f7adda427988092771
RedbackThomson <redbackthomson@gmail.com>
2016-12-31 15:27:56 +0800

    Expand coverage to codebase

--- karma.conf.js
+++ karma.conf.js
@@ -47,6 +47,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'dist/app/**/*.js': ['coverage'],
       '__tests__/**/*[sS]pec.js': ['coverage']
     },
 
